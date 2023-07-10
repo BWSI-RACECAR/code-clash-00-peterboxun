f1651bb29c092225c2912c109319f99d7e192177
peterboxun <128928752+peterboxun@users.noreply.github.com>
2023-07-10 19:41:25 -0400

    Add files via upload

---
+++ .github/Untitled.py
@@ -0,0 +1,13 @@
+
+
+def helloworld(self, string):
+    def main():
+        tc1 = Solution()
+        string1= input()
+        ans = tc1.helloworld(string1)
+        print(ans)
+
+    if __name__ == "__main__":
+        main()
+
+
